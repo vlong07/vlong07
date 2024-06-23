559225352d6ca022c4d59c2ad186bdddd2eabe60
vlong07 <145453250+vlong07@users.noreply.github.com>
2024-06-23 19:17:38 +0700

    Update govip.py

--- govip.py
+++ govip.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-# ENCODE BY KHANHNGUYEN9872                                 # Write your comment here! 
+# ENCODE BY KHANHNGUYEN9872                                 # Write your cohmment here! 
 # Version OBF: v2.6.2                                       # Write your comment here! 
 # Botname: @khanhnguyen9872_bot                             # Write your comment here! 
 # Username Obf: @Vlongzzzz (5723089181)                     # Write your comment here! 
